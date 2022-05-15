8c9685988df80c617ddbd2f1030fb82c0175ee08
muhammad khoirudin <muhammad.khoirudin@infolog.com.sg>
2022-05-15 21:15:14 +0700

    create baseline template

---
+++ resources/views/layouts/inc/ext-js.blade.php
@@ -0,0 +1,8 @@
+<!-- jQuery -->
+<script src="{{url('/adminlte/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ url('/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ url('/adminlte/dist/js/adminlte.min.js')}}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{ url('/adminlte/dist/js/demo.js') }}"></script>
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 {{ isset($title) ? ' | '.$title : '' }}</title>
+
+  @include('layouts.inc.ext-css')
+  @stack('css')
+</head>
+<body class="hold-transition sidebar-mini">
+<!-- Site wrapper -->
+<div class="wrapper">
+  <!-- Navbar -->
+  @include('layouts.inc.navbar')
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  @include('layouts.inc.sidebar')
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      @yield('content-header')
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      @yield('content')
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  @include('layouts.inc.footer')
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+    @include('layouts.inc.ext-js')
+    @stack('js')
+</body>
+</html>
